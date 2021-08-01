d214e5afdf36699928176efe9bfbf572a350fc1d
Marcelo-Diament <marcelodiament@gmail.com>
2021-08-01 19:32:37 -0300

    feature: server routes file

---
+++ server/routes/index.js
@@ -0,0 +1,16 @@
+// Importa o pacote node express
+const express = require('express')
+// Define router a partir do Router do express
+const router = express.Router()
+// Importa o arquivo de controladores '../controllers/index.js'
+const controller = require('../controllers')
+
+// Rota para sortear grupos e listar grupos sorteados
+router.get('/sortear-grupos', controller.sortearGrupos)
+// Rota para listar todos os grupos salvos
+router.get('/grupos', controller.listarGrupos)
+// Rota para listar todos os alunos
+router.get('/alunos', controller.listarAlunos)
+
+// Exporta o módulo atual (router)
+module.exports = router
