2c5dca662e8ca32ff3b820c25cf1f360d95443f3
Marcelo-Diament <marcelodiament@gmail.com>
2021-08-01 20:59:31 -0300

    feature: update alumni limit per group

--- server/config.js
+++ server/config.js
@@ -8,7 +8,7 @@ const config = {}
 config.alunos = alunos
 
 // Número de alunos por grupo
-config.alunosPorGrupo = 6
+config.alunosPorGrupo = 5
 
 // Quantidade de grupos (considerando o número de alunos por grupo pré definido) - alguns grupos terão um aluno a mais
 config.gruposQnt = Math.floor(alunos.length / config.alunosPorGrupo)
